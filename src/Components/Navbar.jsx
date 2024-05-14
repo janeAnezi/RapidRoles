d54edad71192ad64a83756f07626c09fe08cdffe
Anezijane <ahnnieanezi@gmail.com>
2024-05-14 23:25:01 +0100

    add hover link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar  () {
         <>
             <div className=" bg-inherit border border-sky-500  text-white rounded-lg">
                 <div className="flex justify-between p-2">
-                    <h1 className="text-2xl font-bold">RapidRoles</h1>
+                    <h1 className="text-2xl font-bold text-sky-950">RapidRoles</h1>
                     <div >
                         <ul className="flex space-x-6">
                             <li className="hover:bg-sky-500 py-1 px-2 rounded-lg cursor-pointer">Home</li>
@@ -12,7 +12,7 @@ export default function Navbar  () {
                             <li className="hover:bg-sky-500 py-1 px-2 rounded-lg cursor-pointer">Jobs</li>
                             <li className="hover:bg-sky-500 py-1 px-2 rounded-lg cursor-pointer">Services</li>
                             <li className="hover:bg-sky-500 py-1 px-2 rounded-lg cursor-pointer">Blog</li>
-                            <li className="bg-sky-500 hover:bg-sky-500 py-1 px-2 rounded-lg font-bold cursor-pointer">Post Jobs</li>
+                            <li className="bg-sky-500 hover:bg-sky-600 py-1 px-2 rounded-lg font-bold cursor-pointer">Post Jobs</li>
                         </ul>
                     </div>
                     <div>
