11ea2b6832c706b28ca9ef82adc6c81356c6ae1a
Anezijane <ahnnieanezi@gmail.com>
2024-05-14 23:20:10 +0100

    add nav bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,20 +2,21 @@
 export default function Navbar  () {
     return (
         <>
-            <div className=" bg-green-900 text-white rounded-lg">
+            <div className=" bg-inherit border border-sky-500  text-white rounded-lg">
                 <div className="flex justify-between p-2">
                     <h1 className="text-2xl font-bold">RapidRoles</h1>
                     <div >
                         <ul className="flex space-x-6">
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Jobs</li>
-                            <li>Services</li>
-                            <li>Blog</li>
-                            <li>Post Jobs</li>
+                            <li className="hover:bg-sky-500 py-1 px-2 rounded-lg cursor-pointer">Home</li>
+                            <li className="hover:bg-sky-500 py-1 px-2 rounded-lg cursor-pointer">About</li>
+                            <li className="hover:bg-sky-500 py-1 px-2 rounded-lg cursor-pointer">Jobs</li>
+                            <li className="hover:bg-sky-500 py-1 px-2 rounded-lg cursor-pointer">Services</li>
+                            <li className="hover:bg-sky-500 py-1 px-2 rounded-lg cursor-pointer">Blog</li>
+                            <li className="bg-sky-500 hover:bg-sky-500 py-1 px-2 rounded-lg font-bold cursor-pointer">Post Jobs</li>
                         </ul>
                     </div>
                     <div>
+                        <i>searchicon  </i>
                         <i>bellicon  </i>
                         <i>personicon</i>
                     </div>
