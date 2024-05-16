469fd561ce963da91e432732f07195631e838ad4
Anezijane <ahnnieanezi@gmail.com>
2024-05-16 18:34:00 +0100

    add opportunities component

---
+++ src/Components/Opportunities.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Opportunities() {
+    return (
+      <>
+        <div className="py-20 bg-sky-100">
+          <h1 className="text-center text-3xl font-bold bg-gradient-to-b from-sky-400 to-sky-950 text-transparent bg-clip-text">OPPORTUNITIES</h1>
+        </div>
+      </>
+    )
+}
+
