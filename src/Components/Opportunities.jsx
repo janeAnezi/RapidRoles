ac4616dfc24a52894d37af646dc5517a1330a262
Anezijane <ahnnieanezi@gmail.com>
2024-05-16 19:39:37 +0100

    install material ui

--- src/Components/Opportunities.jsx
+++ src/Components/Opportunities.jsx
@@ -1,10 +1,12 @@
-
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 export default function Opportunities() {
     return (
       <>
         <div className="py-20 bg-sky-100">
           <h1 className="text-center text-3xl font-bold bg-gradient-to-b from-sky-400 to-sky-950 text-transparent bg-clip-text">OPPORTUNITIES</h1>
+          <Button variant="contained">Hello world</Button>
         </div>
       </>
     )
